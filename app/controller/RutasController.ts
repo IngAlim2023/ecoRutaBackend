6529ac521c30ef62bef49dcf5540546cca2e6311
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-08-09 21:49:51 -0500

    CRUD Rutas terminado

---
+++ app/controller/RutasController.ts
@@ -0,0 +1,96 @@
+import RutasServices from '#services/RutasServices'
+import { HttpContext } from '@adonisjs/core/http'
+
+const rutas = new RutasServices()
+
+export default class RutasController {
+  async createRoute({ request, response }: HttpContext) {
+    try {
+      const {
+        origen_latitud,
+        origen_longitud,
+        destino_latitud,
+        destino_longitud,
+        transporte,
+        fecha_created_at,
+      } = request.body()
+
+      const data = {
+        origen_latitud,
+        origen_longitud,
+        destino_latitud,
+        destino_longitud,
+        transporte,
+        fecha_created_at,
+      }
+
+      const newRouta = await rutas.create(data)
+
+      return response.status(201).json({ msg: 'Creado', data: newRouta })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+  async readAllRoutes({ response }: HttpContext) {
+    try {
+      const nRutas = await rutas.read()
+      return response.status(200).json({ msg: 'Informacion obtenida', data: nRutas })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+  async readRouteById({ params, response }: HttpContext) {
+    try {
+      const { id } = params
+      const nRutas = await rutas.readById(id)
+      return response.status(200).json({ msg: 'Informacion obtenida', data: nRutas })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+  async Route({ params, response }: HttpContext) {
+    try {
+      const { id } = params
+      await rutas.delete(id)
+      return response.status(200).json({ msg: 'Eliminado' })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+  async updateRoute({ params, request, response }: HttpContext) {
+    try {
+      const { id } = params
+      const {
+        origen_latitud,
+        origen_longitud,
+        destino_latitud,
+        destino_longitud,
+        transporte,
+        fecha_created_at,
+      } = request.body()
+
+      const data = {
+        origen_latitud,
+        origen_longitud,
+        destino_latitud,
+        destino_longitud,
+        transporte,
+        fecha_created_at,
+      }
+
+      const newRouta = await rutas.update(data, id)
+      return response.status(200).json({ msg: 'Actualizado', data: newRouta })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+  async deleteRoute({ params, response }: HttpContext) {
+    try {
+      const { id } = params
+      await rutas.delete(id)
+      return response.status(200).json({ msg: 'Eliminado' })
+    } catch (e) {
+      return response.status(500).json({ msg: 'Error interno.', error: e })
+    }
+  }
+}
